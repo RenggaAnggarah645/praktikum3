fc99d6f272454b97a3a1ee7eac5a0b8c086b3b20
RenggaAnggarah645 <131599995+RenggaAnggarah645@users.noreply.github.com>
2023-09-20 07:45:00 +0700

    Create kode direktur bank.sql

---
+++ kode direktur bank.sql
@@ -0,0 +1,8 @@
+CREATE TABLE DIREKTUR_BANK (
+    Id_direktur INT(20) PRIMARY KEY,
+    Nama_direktur VARCHAR(30),
+    Email VARCHAR(30),
+    Tanggal_mulai_menjabat DATETIME,
+    Nomor_telepon INT(20),
+    Kode_bank INT(20)
+);
