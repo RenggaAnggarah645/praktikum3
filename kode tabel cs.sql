9ae02fdc28d0d4dcd312a847fe1e66527cc45aab
RenggaAnggarah645 <131599995+RenggaAnggarah645@users.noreply.github.com>
2023-09-20 07:42:48 +0700

    Create kode tabel cs.sql

---
+++ kode tabel cs.sql
@@ -0,0 +1,8 @@
+CREATE TABLE CS (
+    Id_cs INT(20) PRIMARY KEY,
+    Nama_cs VARCHAR(30),
+    Alamat VARCHAR(30),
+    Email VARCHAR(30),
+    Nomor_telepon INT(20),
+    Nomor_pegawai INT(10)
+);
